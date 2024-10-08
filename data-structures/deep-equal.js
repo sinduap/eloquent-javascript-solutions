9421e17b6d675bbce263d51299f695a6461ed781
Sindu Andita Pratama <sindu.andita@gmail.com>
2024-10-08 15:03:51 +0700

    Refactor deep-equal

--- data-structures/deep-equal.js
+++ data-structures/deep-equal.js
@@ -18,24 +18,20 @@ export default function deepEqual(value1, value2) {
   const isPrimitive = !(value1 instanceof Object);
   if (isPrimitive) return Object.is(value1, value2) || value1 === value2;
 
-  const isBothArray = Array.isArray(value1) && Array.isArray(value2);
+  const value1Keys = Object.keys(value1),
+    value2Keys = Object.keys(value2);
 
-  if (isBothArray && value1.length === value2.length) {
-    return value1.every((val, i) => deepEqual(val, value2.at(i)));
-  }
+  if (value1Keys.length !== value2Keys.length) return false;
 
-  const isBothObject =
-    Object.getPrototypeOf(value1) === Object.prototype &&
-    Object.getPrototypeOf(value2) === Object.prototype;
-
-  if (
-    isBothObject &&
-    Object.keys(value1).length === Object.keys(value2).length
-  ) {
-    return Object.entries(value1).every(([key, val]) =>
-      deepEqual(val, value2[key])
+  if (Array.isArray(value1)) {
+    return (
+      Array.isArray(value2) &&
+      value1.every((val, i) => deepEqual(val, value2[i]))
     );
   }
 
-  return false;
+  return (
+    Object.getPrototypeOf(value1) === Object.getPrototypeOf(value2) &&
+    Object.entries(value1).every(([key, val]) => deepEqual(val, value2[key]))
+  );
 }
